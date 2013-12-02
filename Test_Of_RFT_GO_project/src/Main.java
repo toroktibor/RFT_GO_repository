ca28a3b9375f8e05a344c369b51f62532a9b9d5d
Tibor Török <trktiti@gmail.com>
2013-12-02 13:09:45 +0100

    asdasdasd

--- Test_Of_RFT_GO_project/src/Main.java
+++ Test_Of_RFT_GO_project/src/Main.java
@@ -1,14 +1,15 @@
 
 public class Main {
 	
-	public String testString;
+	public static String testString;
 	
-	public void everythingIsOK() {
-		this.testString = "Everything is OK, my Dear Friend";
+	public static void everythingIsOK() {
+		testString = "Everything is OK, my Dear Friend";
 		return;
 	}
 	
 	public static void main(String[] args) {
+		everythingIsOK();
 		System.out.println("Fuck yeah! :)");
 	}
 }
