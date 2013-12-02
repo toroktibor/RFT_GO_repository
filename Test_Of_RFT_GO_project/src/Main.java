fd2f8faffdc519875a5e97e196b2841524be5e47
Tibor Török <trktiti@gmail.com>
2013-12-02 13:04:25 +0100

    Main class committed

---
+++ Test_Of_RFT_GO_project/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	
+	public String testString;
+	
+	public void everythingIsOK() {
+		this.testString = "Everything is OK, my Dear Friend";
+		return;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Fuck yeah! :)");
+	}
+}
