213bd2b24117c668d8ae67221569723767acbf5a
olvjoz <olvjoz@gmail.com>
2013-12-04 17:01:58 +0100

    some implements

--- RFT_GO/src/client/Controller.java
+++ RFT_GO/src/client/Controller.java
@@ -1,6 +1,53 @@
 package client;
 
-public class Controller {
-	String myName;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Controller extends Thread {
+	public String myName;
+	private Socket s;
+	private DataInputStream in;
+	private DataOutputStream out;
 	
+	public Controller(){}
+
+    
+	public boolean login(){ 
+		
+		return false;
+	}
+	
+	public void buyHouse(){
+		
+	}
+	
+	public void buyCar(){
+		
+	}
+	
+	public void buyFurniture(String furniture){
+		
+	}
+	
+	public boolean makeInsurances(){
+		return false;
+	}
+
+	public boolean getInitialMessage(){
+		return false;
+	}
+	
+	public boolean getMessageForRead(){
+		return false;
+	}
+	
+	public boolean getGameState(){
+		return false;
+	}
+
+	public void giveUpAndExit(){
+		
+	}
 }
