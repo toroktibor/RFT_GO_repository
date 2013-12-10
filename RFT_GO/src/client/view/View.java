3d62339a58520b6b63549529049f0fd020bbc490
olvjoz <olvjoz@gmail.com>
2013-12-10 23:47:56 +0100

    Konzol View ROLI mikor akarsz csinálni valamit?

--- RFT_GO/src/client/view/View.java
+++ RFT_GO/src/client/view/View.java
@@ -1,8 +1,12 @@
 package client.view;
 
 import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 
@@ -38,36 +42,65 @@ public class View implements IView{
 	}
 	
 	public List<String> getLoginInfos() {
+		Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter Your Name:");
+		String name = sc.nextLine();
+		System.out.println("Enter Server Host:");
+		String host = sc.nextLine();
+		System.out.println("Enter Server Port:");
+		String port = sc.nextLine();
 		
 		/*példa visszatérési értékek, ezek ugye a bekértek lesznek*/
 			List<String> l=new ArrayList<String>();
-			l.add("Jani");
-			l.add("localhost");
-			l.add("7777");
+			l.add(name);
+			l.add(host);
+			l.add(port);
 			return l;
 		/*--*/
 	}
 	
 	
 	public int getBuyingInfos(String s){
-		
-			return 0;
+		Scanner sc = new Scanner(System.in);
+	    System.out.println(s);
+	    String st=sc.nextLine();
+	    if (st=="h")
+	    	return 1;
+	    else if(st=="k")
+	    	return 2;
+	    else
+		return 0;
 	}
 	
 	
 	public int getInsurances(String s){
-		
+		Scanner sc = new Scanner(System.in);
+	    System.out.println(s);
+	    String st=sc.nextLine();
+	    if (st=="a")
+	    	return 1;
+	    else if(st=="h")
+	    	return 2;
+	    else if(st=="m")
+	    	return 3;
+	    else
 		return 0;
 	}
 	
 	
 	public int getFurnitureOptions(String s){
+		Scanner sc = new Scanner(System.in);
+	    System.out.println(s);
+	    String st=sc.nextLine();
+	    if (st=="i")
+	    	return 1;
+	    else
 		return 0;
 	}
 
 	
 	public void simpleMessage(String s) {
-		
+		System.out.println(s);
 	}
 	
 
