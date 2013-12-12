cc67c99203bfd7335348ede3f05d2cf9dbe8c321
Tibor Török <trktiti@gmail.com>
2013-12-12 15:28:54 +0100

    Kibővetve kivételdobásokkal.

--- RFT_GO/src/server/IGamePlay.java
+++ RFT_GO/src/server/IGamePlay.java
@@ -6,9 +6,9 @@ import java.lang.reflect.InvocationTargetException;
 public interface IGamePlay {
 	public void dice() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException;
 	public void executeFieldCommand() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException;
-	public void executeLuckyCardCommand();
+	public void executeLuckyCardCommand() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException;
 	public void init();
-	public void sendGameState();
+	public void sendGameState(String option) throws IOException;
 	public void startGame() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException;
 	public void waitForPlayers(int maxNumberOfPlayers) throws IOException;
 }
