bca48d1d5453731443a9af9ff7209afa68cb1274
githubosito <fakeezel@gmail.com>
2013-12-14 02:07:15 +0100

    GazdOkGUIConf osztály definiálása és a GUI finomítása.

--- RFT_GO/src/client/view/View.java
+++ RFT_GO/src/client/view/View.java
@@ -1,168 +1,289 @@
+//15:42
 package client.view;
 
+import java.awt.BorderLayout;
+
+import client.view.GazdOkGUIConf;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.LayoutManager;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
 
 import client.Controller;
 import client.StateOfPlayer;
 
-public class View implements IView{
+public class View extends JFrame implements IView {
 
-	private JFrame frame;
+	private static final long serialVersionUID = 1L;
+	private static final String ENDL = "\n";
+	private static int noHistoryEntries = 10;
+	private JPanel pnlContentPane;
+	private JPanel pnlGameTable;
+
+	private JPanel pnlHistory;
+	private JTextArea historyText;
+	private List<String> historyEntries = new ArrayList<String>(
+			View.noHistoryEntries);
+	private JPanel pnlStatus;
 
 	public View() {
 		initialize();
+//		foo();
+		simpleMessage("Remove entry");
 	}
 
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// simpleMessage("Welcome!");
+		// getFurnitureOptions("akarjadm�");
+		initPnlGameTable();
+		initPnlHistory();
+		initPnlStatus();
+
+		initPnlContentpane();
+		initFrame();
+ 
 	}
 
-	
-		public void showView() {
+	private void initPnlGameTable() {
+		pnlGameTable = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.weightx = 14;
+		gbc.weighty = 9;
+		
+		int noField = 0;
+		for (int i = 0; i < 14; i++) {
+			gbc.weightx = i;
+			pnlGameTable.add(new JButton(Integer.toString(noField++)), gbc);
+		}
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 14; j++){
+				if(i == 0 || i == 8){
+					gbc.gridx = j;
+					gbc.gridy = i;
+					
+				}
+				else if(j > 0 && j < 8){
+					gbc.gridx = j;
+					gbc.gridy = i;
+				}
+				noField++;
+			}
+			
+		}
+		
+
+		gbc.gridy = 0;
+		
+//		pnlGameTable.add(new JLabel("-Game table-"));
+		pnlGameTable.setPreferredSize(GazdOkGUIConf.GAMETABLE_PREF_DIM);
+		pnlGameTable.setBorder(BorderFactory
+				.createBevelBorder(BevelBorder.RAISED));
+		pnlGameTable.setBackground(GazdOkGUIConf.GAMETABLE_BGCOLOR);
+
+	}
+
+	private void initPnlHistory() {
+
+		pnlHistory = new JPanel(new GridBagLayout());
+
+		pnlHistory.setPreferredSize(GazdOkGUIConf.HISTORY_PREF_DIM);
+		pnlHistory.setBackground(GazdOkGUIConf.HISTORY_BGCOLOR);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.weightx = 1;
+		gbc.weighty = 2;
+		gbc.fill = GridBagConstraints.BOTH;
+		
+		gbc.anchor = GridBagConstraints.CENTER;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+
+		Dimension labelHistoryDim = new Dimension();
+		labelHistoryDim.width = GazdOkGUIConf.HISTORY_PREF_DIM.width;
+		labelHistoryDim.height = 20;
+		
+		JLabel labelHistory = new JLabel("-history-");
+		labelHistory.setPreferredSize(labelHistoryDim);
+		pnlHistory.add(labelHistory, gbc);
+		
+		
+		historyText = new JTextArea(20, 30);
+		JScrollPane historyScrollPane = new JScrollPane(historyText);
+		historyScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		historyScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		historyText.setEditable(false);
+		historyText.setLineWrap(true);
+		historyText.setWrapStyleWord(true);
+		historyText.setAutoscrolls(true);
+
+		Dimension historyScrollPaneDim = new Dimension();
+		historyScrollPaneDim.width = GazdOkGUIConf.HISTORY_PREF_DIM.width;
+		historyScrollPaneDim.height = GazdOkGUIConf.HISTORY_PREF_DIM.height-20;
+		historyScrollPane.setPreferredSize(historyScrollPaneDim);
+
+		gbc.anchor = GridBagConstraints.PAGE_END;
+		gbc.gridy = 1;
+		pnlHistory.add(historyScrollPane, gbc);
+		
+		
+		simpleMessage("A j�t�k elkezd�d�tt.\tLegyen �n is milliomos!");
+	}
+
+	private void initPnlStatus() {
+		pnlStatus = new JPanel();
+		pnlStatus.add(new JLabel("-statusbar-"));
+		pnlStatus.setPreferredSize(GazdOkGUIConf.STATUSBAR_PREF_DIM);
+		pnlStatus.setBackground(GazdOkGUIConf.STATUSBAR_BGCOLOR);
+//		pnlStatus.add(new JButton("Statusbar"));
+	}
+
+	private void initPnlContentpane() {
+		pnlContentPane = new JPanel(new GridBagLayout());
+		pnlContentPane.setBackground(GazdOkGUIConf.CONTENTPANE_BGCOLOR);
+		pnlContentPane.setVisible(true);
+		pnlContentPane.setPreferredSize(GazdOkGUIConf.WINDOW_PREF_DIM);
+
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.weightx = 2;
+		gbc.weighty = 2;
+		gbc.fill = GridBagConstraints.BOTH;
+
+		gbc.anchor = GridBagConstraints.NORTHEAST;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		pnlContentPane.add(pnlGameTable, gbc);
+
+		gbc.gridx = 1;
+		pnlContentPane.add(pnlHistory, gbc);
+
+		gbc.anchor = GridBagConstraints.PAGE_END;
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = 2;
+		pnlContentPane.add(pnlStatus, gbc);
+	}
+
+	private void initFrame() {
+		setTitle("Gazd�lkodj okosan!");
+		setLocation(0, 0);
+		// setResizable(false);
+		setContentPane(pnlContentPane);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+	}
+
+	public void showView() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					View window = new View();
-					window.frame.setVisible(true);
+					window.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-		
-	
+
 	public List<String> getLoginInfos() {
 		Scanner sc = new Scanner(System.in);
-	    System.out.println("Enter Your Name:");
+		System.out.println("Enter Your Name:");
 		String name = sc.nextLine();
 		System.out.println("Enter Server Host:");
 		String host = sc.nextLine();
 		System.out.println("Enter Server Port:");
 		String port = sc.nextLine();
-		
-		/*példa visszatérési értékek, ezek ugye a bekértek lesznek*/
-			List<String> l=new ArrayList<String>();
-			l.add(name);
-			l.add(host);
-			l.add(port);
-			return l;
+
+		/* p�lda visszat�r�si �rt�kek, ezek ugye a bek�rtek lesznek */
+		List<String> l = new ArrayList<String>();
+		l.add(name);
+		l.add(host);
+//		l.add(port);
+		return l;
 		/*--*/
 	}
-	
-	
-	public int getBuyingInfos(String s, boolean b){
-		if (b==true){Object[] options = {"Nem kell",
-        "Hitel","Készpénz"};
-		int n = JOptionPane.showOptionDialog(frame,
-				s,
-				"Vásárlás",
-				JOptionPane.YES_NO_OPTION,
-				JOptionPane.QUESTION_MESSAGE,
-				null,    
-				options,  
-				options[0]); 
-		return n;}else{		Object[] options = {"Nem kell",
-		        "Hitel"};
-				int n = JOptionPane.showOptionDialog(frame,
-						s,
-						"Vásárlás",
-						JOptionPane.YES_NO_OPTION,
-						JOptionPane.QUESTION_MESSAGE,
-						null,    
-						options,  
-						options[0]); 
-				return n;}
-		/*Scanner sc = new Scanner(System.in);
-	    String st=sc.nextLine();
-	    if (st.equals("h")){
-	    	return 1;}
-	    else if(st.equals("k")){
-	    	return 2;}
-	    else{
-		return 0;
-		}*/
+
+	public int getBuyingInfos(String s, boolean c) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(s);
+		String st = sc.nextLine();
+		if (st == "h")
+			return 1;
+		else if (st == "k")
+			return 2;
+		else
+			return 0;
 	}
-	
-	
-	public int getInsurance(String s){
-		Object[] options = {"Megköt",
-        "Nem köt!"};
-		int n = JOptionPane.showOptionDialog(frame,
-				s,
-				"Biztosítás",
-				JOptionPane.YES_NO_OPTION,
-				JOptionPane.QUESTION_MESSAGE,
-				null,    
-				options,  
-				options[0]); 
-		return n;
-		
-		/*Scanner sc = new Scanner(System.in);
-	    String st=sc.nextLine();
-	    if (st.equals("a")){
-	    	return 1;}
-	    else if(st.equals("h")){
-	    	return 2;}
-	    else if(st.equals("m")){
-	    	return 3;}
-	    else{
-		return 0;}*/
+
+	public int getInsurance(String s) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(s);
+		String st = sc.nextLine();
+		if (st == "a")
+			return 1;
+		else if (st == "h")
+			return 2;
+		else if (st == "m")
+			return 3;
+		else
+			return 0;
 	}
-	
-	
-	public int getFurnitureOptions(String s){
-		Object[] options = {"Megvásárol",
-        "Nem kérem!"};
-		int n = JOptionPane.showOptionDialog(frame,
-				s,
-				"Berendezés",
-				JOptionPane.YES_NO_OPTION,
-				JOptionPane.QUESTION_MESSAGE,
-				null,    
-				options,  
-				options[0]); 
+
+	public int getFurnitureOptions(String s) {
+		Object[] options = { "Megv�s�rol", "Nem k�rem!" };
+		int n = JOptionPane.showOptionDialog(this, s, "Berendez�s v�s�rl�s",
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+				options, options[0]);
 		return n;
 	}
 
-	
-	public String simpleMessage(String s) {
-		Object[] options = {"OK"};
-				int n = JOptionPane.showOptionDialog(frame,
-						s,
-						"Üzenet",
-						JOptionPane.YES_NO_OPTION,
-						JOptionPane.QUESTION_MESSAGE,
-						null,    
-						options,  
-						options[0]); 
-		
-		return String.valueOf(n);
+	public String simpleMessage(String message) {
+		historyText.append(new SimpleDateFormat("[HH:mm:ss").format(Calendar
+				.getInstance().getTime()) + "] : " + message + ENDL);
+		return message;
 	}
-	
 
-	
 	public void refreshView() {
-		
-		List<StateOfPlayer> sp=Controller.getGameState();
-		for (StateOfPlayer stateOfPlayer : sp) {
-			if (stateOfPlayer.getIdNumber()==Controller.getMyID()){
-				System.out.println(stateOfPlayer.getLocation());
-			}
-		}
+
+		List<StateOfPlayer> sp = Controller.getGameState();
+
 	}
-	
-	
-	
+
+	private void foo() {
+		Object[] possibilities = { "ham", "spam", "yam" };
+		String s = (String) JOptionPane.showInputDialog(pnlContentPane,
+				"Complete the sentence:\n" + "\"Green eggs and...\"",
+				"Customized Dialog", JOptionPane.PLAIN_MESSAGE, null,
+				possibilities, "ham");
+	}
+
 }
